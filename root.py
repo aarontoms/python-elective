663232d3672f32e1c2a0969fe5e4d21124302ca5
Aaron Tom Viji <aarontoms29@gmail.com>
2025-01-19 22:31:01 +0530

    square_root

--- root.py
+++ root.py
@@ -1,4 +1,11 @@
-from math import sqrt
+def estimate_sqrt(n):
+    estimate = n / 2.0
+    precision = 0.0000001
+    while True:
+        final = (estimate + n / estimate) / 2.0
+        if abs(estimate - final) < precision:
+            return final
+        estimate = final
 
 while True:
     num = float(input("Enter a number: "))
@@ -6,5 +13,5 @@ while True:
     if num < 0:
         print("Enter a positive number")
     else:
-        print("Square root of", num, "is", sqrt(num))
+        print("Square root of", num, "is", estimate_sqrt(num))
         break
