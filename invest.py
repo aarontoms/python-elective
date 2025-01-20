a7be7e1c230d41e8bc2f3da8b887ddb8a8e5a993
Aaron Tom Viji <aarontoms29@gmail.com>
2025-01-20 21:11:22 +0530

    investment_report

--- invest.py
+++ invest.py
@@ -1,4 +1,17 @@
-invest = float(input("Enter the amount of the investment: "))
-return_amount = float(input("Enter the amount of the return: "))
-ret = (return_amount - invest)/invest
-print("The Return on investment is", ret)
+intial_amount = float(input("Enter the initial amount: "))
+rate = float(input("Enter the interest rate: "))
+years = int(input("Enter the number of years: "))
+
+init = intial_amount
+final = intial_amount
+rate = rate / 100
+
+print("Year  \tIntial_Amount  \tInterest  \tfinal")    
+for i in range(1, years+1):
+    final += round(rate*intial_amount, 2)
+   
+    print(i, "\t", intial_amount, "\t", rate, "\t\t", final)
+    intial_amount = final
+print()
+print("The final amount after", years, "years is", final)
+print("The interest earned after", years, "years is", round(final-init,2))
