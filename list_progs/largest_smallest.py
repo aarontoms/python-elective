1f3f85de915bfbd7496504f882a5bbbb9f1efb6b
aarontoms <aarontoms29@gmail.com>
2025-02-03 21:14:40 +0530

    list_programs

---
+++ list_progs/largest_smallest.py
@@ -0,0 +1,13 @@
+l = []
+print("\nEnter numbers to add to list and Enter blank to stop")
+while True:
+    try:
+        num = int(input(">>> "))
+    except:
+        break
+    l.append(num)
+    
+
+print("\nList:", l)
+print("\nLargest number:", max(l))
+print("Smallest number:", min(l))
